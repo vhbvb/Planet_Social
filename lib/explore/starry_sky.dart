5430ab4fb45031e0f26cbbdb72294eb5893f79dd
qinch <qinch@yoozoo.com>
2019-12-30 10:43:33 +0800

    fix 双击重制bug

--- lib/explore/starry_sky.dart
+++ lib/explore/starry_sky.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:planet_social/explore/star_style.dart';
 
 class StarrySky extends StatefulWidget {
-
   StarrySky({Key key, this.onScroll, this.stars}) : super(key: key);
   final Function(Offset) onScroll;
   final List stars;
@@ -13,8 +12,7 @@ class StarrySky extends StatefulWidget {
   State<StatefulWidget> createState() => _StarrySkyState();
 }
 
-class _StarrySkyState extends State<StarrySky> with TickerProviderStateMixin{
-
+class _StarrySkyState extends State<StarrySky> with TickerProviderStateMixin {
   Offset _offset = Offset(0, 0);
   Offset _cachedOffset = Offset(0, 0);
   List _gridViews;
@@ -28,68 +26,77 @@ class _StarrySkyState extends State<StarrySky> with TickerProviderStateMixin{
   double get ch => MediaQuery.of(context).size.height;
 
   _background() => GestureDetector(
-      onDoubleTap: (){
-        _jumpTo(Offset(0,0));
+      onDoubleTap: () {
+        _jumpTo(Offset(0, 0));
       },
-      onPanStart: (start) {
-      },
+      onPanStart: (start) {},
       onPanCancel: () {},
       onPanEnd: (end) {
-        _controller.reset();
-        double x= end.velocity.pixelsPerSecond.dx;
-        double y= end.velocity.pixelsPerSecond.dy;
-        if(x == 0 && y == 0 && !_controller.isCompleted) return;
-        Animation curve = CurvedAnimation(parent: _controller,curve: Curves.easeOut);
-        _animation = Tween(begin: Offset(x,y),end: Offset(0, 0)).animate(curve)..addListener((){
-          _needScroll(Offset(_animation.value.dx * 0.001,_animation.value.dy * 0.001));
-        })..addStatusListener((status){
-
-          if (AnimationStatus.completed == status){
-            // _controller.reset();
+        if (_controller == null || _controller.isCompleted) {
+          if (_controller != null) {
+            _controller.dispose();
           }
-        });
-        _controller.forward();
+          _controller = AnimationController(
+              duration: Duration(milliseconds: _animationTime), vsync: this);
+          double x = end.velocity.pixelsPerSecond.dx;
+          double y = end.velocity.pixelsPerSecond.dy;
+          if (x == 0 && y == 0 && !_controller.isCompleted) return;
+          Animation curve =
+              CurvedAnimation(parent: _controller, curve: Curves.easeOut);
+          _animation =
+              Tween(begin: Offset(x, y), end: Offset(0, 0)).animate(curve)
+                ..addListener(() {
+                  _needScroll(Offset(_animation.value.dx * 0.001,
+                      _animation.value.dy * 0.001));
+                })
+                ..addStatusListener((status) {
+                  if (AnimationStatus.completed == status) {
+                    // _controller.reset();
+                  }
+                });
+          _controller.forward();
+        }
       },
       onPanUpdate: (detail) {
         _needScroll(detail.delta);
       },
       child: OverflowBox(
         alignment: Alignment.center,
-        maxHeight: ch *3,
-        maxWidth: cw *3,
+        maxHeight: ch * 3,
+        maxWidth: cw * 3,
         child: Stack(
           children: _grids(),
         ),
       ));
 
-  _needScroll(Offset offset){
-        double x = _offset.dx + offset.dx;
-        double y = _offset.dy + offset.dy;
-        _offset = Offset(x, y);
+  _needScroll(Offset offset) {
+    double x = _offset.dx + offset.dx;
+    double y = _offset.dy + offset.dy;
+    _offset = Offset(x, y);
 
-        double cx = _cachedOffset.dx + offset.dx;
-        double cy = _cachedOffset.dy + offset.dy;
+    double cx = _cachedOffset.dx + offset.dx;
+    double cy = _cachedOffset.dy + offset.dy;
 
-        if (cx < 0) {
-          _updateGrids(3);
-          cx = cx + cw;
-        }
-        if (cx > cw) {
-          _updateGrids(1);
-          cx = cx - cw;
-        }
-        if (cy < 0) {
-          _updateGrids(2);
-          cy = cy + ch;
-        }
-        if (cy > ch) {
-          _updateGrids(0);
-          cy = cy - ch;
-        }
-        setState(() {
-          _cachedOffset = Offset(cx, cy);
-          widget.onScroll(_offset);
-        });
+    if (cx < 0) {
+      _updateGrids(3);
+      cx = cx + cw;
+    }
+    if (cx > cw) {
+      _updateGrids(1);
+      cx = cx - cw;
+    }
+    if (cy < 0) {
+      _updateGrids(2);
+      cy = cy + ch;
+    }
+    if (cy > ch) {
+      _updateGrids(0);
+      cy = cy - ch;
+    }
+    setState(() {
+      _cachedOffset = Offset(cx, cy);
+      widget.onScroll(_offset);
+    });
   }
 
   List<Widget> _grids() {
@@ -100,50 +107,58 @@ class _StarrySkyState extends State<StarrySky> with TickerProviderStateMixin{
     for (var i = 0; i < 2; i++) {
       //第几列
       for (var j = 0; j < 2; j++) {
-        
         grids.add(Positioned(
-          top: i * ch+ _cachedOffset.dy,
+          top: i * ch + _cachedOffset.dy,
           left: j * cw + _cachedOffset.dx,
           child: _gridViews[i][j],
         ));
       }
     }
 
-    for (var item in widget.stars) 
-    {
+    for (var item in widget.stars) {
       Offset position = item.position;
-      Offset distance = Offset(position.dx + _offset.dx,position.dy + _offset.dy);
+      Offset distance =
+          Offset(position.dx + _offset.dx, position.dy + _offset.dy);
       double padding = 100;
-      bool left = distance.dx > -(cw/2+padding);
-      bool right = distance.dx < (cw/2+padding);
-      bool top = distance.dy > -(ch/2+padding);
-      bool bottom = distance.dy < (ch/2+padding);
+      bool left = distance.dx > -(cw / 2 + padding);
+      bool right = distance.dx < (cw / 2 + padding);
+      bool top = distance.dy > -(ch / 2 + padding);
+      bool bottom = distance.dy < (ch / 2 + padding);
 
-      if ( left && right && top && bottom ){
+      if (left && right && top && bottom) {
         grids.add(StarStyle(
           model: item,
-          left: distance.dx+3/2*cw,
-          top: distance.dy+3/2*ch,
+          left: distance.dx + 3 / 2 * cw,
+          top: distance.dy + 3 / 2 * ch,
           context: context,
         ));
       }
     }
-    
+
     return grids;
   }
 
 //0 top、1 left、2 bottom、3 right
   _updateGrids(int type) {
-    Widget back = SizedBox(child: Image.asset("assets/star/star.jpeg",fit: BoxFit.fill,),height: ch,width: cw,);
+    Widget back = SizedBox(
+      child: Image.asset(
+        "assets/star/star.jpeg",
+        fit: BoxFit.fill,
+      ),
+      height: ch,
+      width: cw,
+    );
 
     if (_gridViews == null) {
-      _gridViews = [[back, back], 
-                    [back, back]];
+      _gridViews = [
+        [back, back],
+        [back, back]
+      ];
     } else {
       switch (type) {
         case 0:
           _gridViews.removeLast();
-          _gridViews.insert(0, [back,back]);
+          _gridViews.insert(0, [back, back]);
           break;
         case 1:
           _gridViews[0].removeLast();
@@ -153,7 +168,7 @@ class _StarrySkyState extends State<StarrySky> with TickerProviderStateMixin{
           break;
         case 2:
           _gridViews.removeAt(0);
-          _gridViews.add([back,back]);
+          _gridViews.add([back, back]);
           break;
         case 3:
           _gridViews[0].removeAt(0);
@@ -166,34 +181,35 @@ class _StarrySkyState extends State<StarrySky> with TickerProviderStateMixin{
     }
   }
 
-  _jumpTo(Offset offset){
+  _jumpTo(Offset offset) {
+    var x = offset.dx - _offset.dx;
+    var y = offset.dy - _offset.dy;
 
-      var x = offset.dx - _offset.dx;
-      var y = offset.dy - _offset.dy;
+    if (x != 0 && y != 0) {
+      if (_jumpController == null || _jumpController.isCompleted) {
+        if (_jumpController != null) {
+          _jumpController.dispose();
+        }
+        _jumpController = AnimationController(
+            duration: Duration(milliseconds: _animationTime), vsync: this);
 
-      if(x!=0 && y!=0){
-      _jumpController.reset();
-      Animation curve = CurvedAnimation(parent: _jumpController,curve: Curves.easeOut);
-      _jumpAnimation = Tween(begin: _offset,end: offset).animate(curve)..addListener((){
+        Animation curve =
+            CurvedAnimation(parent: _jumpController, curve: Curves.easeOut);
+        _jumpAnimation = Tween(begin: _offset, end: offset).animate(curve)
+          ..addListener(() {
+            var x = _jumpAnimation.value.dx - _offset.dx;
+            var y = _jumpAnimation.value.dy - _offset.dy;
 
-        var x = _jumpAnimation.value.dx - _offset.dx;
-        var y = _jumpAnimation.value.dy - _offset.dy;
-
-        var dis = Offset(x, y);
-          _needScroll(dis);
-        })..addStatusListener((status){
-          if(AnimationStatus.completed == status){
-            // _jumpController.reset();
-          }
-        });
+            var dis = Offset(x, y);
+            _needScroll(dis);
+          });
+        _jumpController.forward();
       }
-      _jumpController.forward();
+    }
   }
 
   @override
   void initState() {
-    _controller = AnimationController(duration: Duration(milliseconds: _animationTime),vsync: this);
-    _jumpController = AnimationController(duration: Duration(milliseconds: _animationTime),vsync: this);
     widget.controller.jumpTo = _jumpTo;
     super.initState();
   }
@@ -211,6 +227,5 @@ class _StarrySkyState extends State<StarrySky> with TickerProviderStateMixin{
 }
 
 class StarrySkyController {
-
   Function(Offset) jumpTo;
 }
